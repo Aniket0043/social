bed269517dc69c907eef063da84fbaf993e9e3d8
Aniket0043 <aniket9350@gmail.com>
2020-12-23 01:17:42 +0530

    add

--- index.js
+++ index.js
@@ -1,7 +1,20 @@
+
 const express=require("express")
 const app=express()
 const path=require("path")
+var bodyParser = require('body-parser')
 port=3000;
+//assests files
+app.use(express.static("/assets"));
+app.set('views',path.join(__dirname,"views"))
+//middleware
+app.use(bodyParser.urlencoded({ extended: true }));
+//database
+//request
+app.get("/",(req,res)=>{
+    res.send("login.html")
+    console.log("aniket")
+})
 app.listen(port,()=>
 {
 console.log("connecting to the sever port")}
